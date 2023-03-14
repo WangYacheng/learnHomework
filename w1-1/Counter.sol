9d20139ea3c695d6947302f8c89a6c4e7ea72308
second <wangyacheng1106@163.com>
2023-03-14 16:45:07 +0800

    1，第一周的俩次作业

---
+++ w1-1/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+contract Counter{
+    uint public X;
+
+    function add(uint _x) external {
+        X=_x;
+    }
+}
+
+
+
+
+// 交易哈希 0x5cc7f2fe095015019502cf8491f559c977a861c2fd699dd181295a808abc9e00
